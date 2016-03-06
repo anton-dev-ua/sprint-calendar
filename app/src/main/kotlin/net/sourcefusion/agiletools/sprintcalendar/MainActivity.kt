646edd73e4384ae388478b2600a1e0efa57917b0
Anton <box.of.anton@gmail.com>
2016-03-06 16:06:33 +0100

    cleanup

--- app/src/main/kotlin/net/sourcefusion/agiletools/sprintcalendar/MainActivity.kt
+++ app/src/main/kotlin/net/sourcefusion/agiletools/sprintcalendar/MainActivity.kt
@@ -22,7 +22,6 @@ class MainActivity : AppCompatActivity() {
 
     val holidayProvider: HolidayProvider by lazy {
         println("lazy holiday provider")
-//        DefaultHolidayProvider(LocalDate(2016, 1, 18))
         Injector.holidayProvider
     }
 
@@ -31,7 +30,6 @@ class MainActivity : AppCompatActivity() {
 
         println("activity on create")
 
-//        sprintCalendar.holidayProvider = DefaultHolidayProvider(LocalDate(2016, 1, 19))
         sprintCalendar.holidayProvider = holidayProvider
         sprintCalendar.initByCurrentDate();
 
@@ -39,112 +37,7 @@ class MainActivity : AppCompatActivity() {
         val ui = CalendarActivityUI(sprintCalendar)
         println("$sprintCalendar")
         ui.setContentView(this)
-        /*activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-
-        ViewGroup rootView = (ViewGroup) findViewById(R.id.rootView);
-
-        attachListenerToAll("teamMemberDayView", rootView,
-                new MemberDayOnLongClickListener(),
-                new MemberDayOnDoubleClickListener());
-
-        attachListenerToAll("dayViewHeader", rootView,
-                new DayHeaderOnLongClickListener(),
-                null);
-
-        TeamMember pavel = new TeamMember("Pavel");
-        pavel.setPresence(new LocalDate(2016, 1, 13), HALF_DAY);
-        pavel.setPresence(new LocalDate(2016, 1, 14), NONE);
-        TeamMember alex = new TeamMember("Alex");
-        alex.setPresence(new LocalDate(2016, 1, 6), NONE);
-        Team team = new Team(
-                alex,
-                new TeamMember("Anton"),
-                new TeamMember("Dima"),
-                new TeamMember("Paulina"),
-                pavel
-        );
-
-        holidayProvider = new DefaultHolidayProvider(new LocalDate(2016, 1, 6));
-        sprintCalendar = new SprintCalendar(
-                team,
-                new DefaultDateProvider(),
-                holidayProvider
-        );
-
-
-        sprintCalendar.initByCurrentDate();
-
-        activityMainBinding.setSprintCalendar(sprintCalendar);*/
 
     }
 
-    /*private void attachListenerToAll(String tag, ViewGroup rootView, View.OnLongClickListener listener, View.OnClickListener listener2) {
-        int childCount = rootView.getChildCount();
-        for (int childIndex = 0; childIndex < childCount; childIndex++) {
-            View view = rootView.getChildAt(childIndex);
-            if (view instanceof ViewGroup) {
-                attachListenerToAll(tag, (ViewGroup) view, listener, listener2);
-            } else if (tag.equals(view.getTag())) {
-                if (listener != null) view.setOnLongClickListener(listener);
-                if (listener2 != null) view.setOnClickListener(listener2);
-            }
-        }
-    }
-
-    private boolean processTeamMemberDayViewClick(View view, PresenceType oppositePresence) {
-        if (view instanceof TeamMemberDayView) {
-            TeamMemberDayView dayView = (TeamMemberDayView) view;
-            TeamMember teamMember = dayView.getTeamMember();
-            SprintDay day = dayView.getDay();
-            PresenceType presence = teamMember.presence(day.getDate());
-            teamMember.setPresence(day.getDate(), presence == FULL_DAY ? oppositePresence : FULL_DAY);
-            activityMainBinding.setSprintCalendar(sprintCalendar);
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    private boolean processDayViewClick(View view) {
-        SprintDay day = (SprintDay) ((View) view.getParent()).getTag();
-        day.setHoliday(!day.isHoliday());
-        activityMainBinding.setSprintCalendar(sprintCalendar);
-        return true;
-    }
-
-    private class MemberDayOnDoubleClickListener implements View.OnClickListener {
-        static final long DOUBLE_CLICK_DELAY = 500;
-        long lastClickTime = 0;
-
-        @Override
-        public void onClick(View view) {
-            long clickTime = System.currentTimeMillis();
-            if (clickTime - lastClickTime < DOUBLE_CLICK_DELAY) {
-                processTeamMemberDayViewClick(view, PresenceType.HALF_DAY);
-            }
-            lastClickTime = clickTime;
-        }
-    }
-
-    private class MemberDayOnLongClickListener implements View.OnLongClickListener {
-        @Override
-        public boolean onLongClick(View view) {
-            return processTeamMemberDayViewClick(view, PresenceType.NONE);
-        }
-    }
-
-    private class DayHeaderOnLongClickListener implements View.OnLongClickListener {
-        @Override
-        public boolean onLongClick(View view) {
-            return processDayViewClick(view);
-        }
-    }*/
 }
-
-/*class SomeActivityUI : AnkoComponent<MainActivity> {
-    override fun createView(ui: AnkoContext<MainActivity>) = with(ui) {
-        linearLayout {
-            textView("Hello")
-        }
-    }
-}*/
